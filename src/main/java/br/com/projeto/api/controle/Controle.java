79872f4efb2518a75331c45d0c9ba18f61a0a5ca
Gabriel Mendonca <gabriel-peres@hotmail.com>
2024-07-10 00:17:55 -0300

    Primeira vinculação ao Angular

--- src/main/java/br/com/projeto/api/controle/Controle.java
+++ src/main/java/br/com/projeto/api/controle/Controle.java
@@ -1,6 +1,8 @@
 package br.com.projeto.api.controle;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,8 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.projeto.api.modelo.Cliente;
 import br.com.projeto.api.repositorio.Repositorio;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @RestController
+@CrossOrigin(origins = "*")
 public class Controle {
     
 @Autowired
@@ -25,6 +31,20 @@ public Iterable<Cliente> selecionar(){
     return acao.findAll();
 }
 
+public String putMethodName(@PathVariable String id, @RequestBody String entity) {
+    //TODO: process PUT request
+    
+    return entity;
+}
+@PutMapping("/")
+public Cliente editar(@RequestBody Cliente c){
+return acao.save(c);
+}
+
+@DeleteMapping("/{codigo}")
+public void remover(@PathVariable long codigo){
+acao.deleteById(codigo);
+}
 
 
 }
